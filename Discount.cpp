1c0e310250106361eedaecc23f9c7ede36c4f8ec
IT21158018 <99158752+IT21158018@users.noreply.github.com>
2022-05-20 15:03:10 -0500

    Update Discount.cpp

--- Discount.cpp
+++ Discount.cpp
@@ -2,17 +2,16 @@
 #include"Payment.h"
 #define size 2
 
-using namespace std;
 
-Discount::Discount(){}
+Discount::Discount(){};
 
 Discount::Discount(int sId, int eId){
 	staffId = sId;
 	eventId = eId;
-}
+};
 
-void Discount::calculateDiscount(){}
+void Discount::calculateDiscount(){};
 
-void Discount::displayDiscount() {}
+void Discount::displayDiscount() {};
 
-Discount:: ~Discount() {}
+Discount:: ~Discount() {};
